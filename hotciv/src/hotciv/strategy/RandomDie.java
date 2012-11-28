fcfa4b0cfc9b81990f359bfe0e57d4a62f3ebef5
Jakob Graugaard Laursen <jakob@Jakobs-MacBook-Air.local>
2012-11-28 11:18:59 +0100

    Random die added

---
+++ hotciv/src/hotciv/strategy/RandomDie.java
@@ -0,0 +1,14 @@
+package hotciv.strategy;
+
+import java.util.Random;
+
+public class RandomDie implements DieRoller {
+	private Random randomGenerator;
+
+	public int rollDie() {
+		// we roll a number between 0 and 5. Add
+		// 1 to get one between 1 and 6;
+		return randomGenerator.nextInt( 5 ) + 1;
+	}
+
+}
